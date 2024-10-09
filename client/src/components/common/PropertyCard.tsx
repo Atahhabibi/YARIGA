8fd9c332fc0f5e280fc57ac3cfb52848c40b0843
atahhabibi <habibiatah@gmail.com>
2024-10-09 13:54:12 -0700

    10/9 1:54

--- client/src/components/common/PropertyCard.tsx
+++ client/src/components/common/PropertyCard.tsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import { Place } from "@mui/icons-material";
+import { Link } from "@pankod/refine-react-router-v6";
+import { PropertyCardProps } from "./../../interfaces/property";
+import {
+  Typography,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Stack
+} from "@mui/material";
 
-const PropertyCard = () => {
+const PropertyCard = ({
+  id,
+  title,
+  location,
+  price,
+  photo
+}: PropertyCardProps) => {
   return (
-    <div>
-      PropertyCard
-    </div>
-  )
-}
+    <Card
+      component={Link}
+      to={`/properties/${id}`}
+      sx={{
+        maxWidth: "330px",
+        padding: "10px",
+        "&:hover": { boxShadow: "0 22px 45px 2px rgba(176,176,176,0.1)" },
+        cursor: "pointer",
+        textDecoration:"none",
+      }}
+      elevation={0}
+    >
+      <CardMedia
+        component="img"
+        width="100%"
+        height={210}
+        image={photo}
+        alt="cart-image"
+        sx={{ borderRadius: "10px" }}
+      />
+      <CardContent
+        sx={{
+          display: "flex",
+          gap: "10px",
+          paddingX: "5px",
+          flexDirection: "row",
+          justifyContent: "space-between"
+        }}
+      >
+        <Stack direction="column" gap={1}>
+          <Typography fontSize={16} fontWeight={500} color="#808191">
+            {title}
+          </Typography>
+          <Stack direction="row" gap={0.5} alignItems="flex-start">
+            <Place sx={{ fontSize: 18, color: "grey", marginTop: 0.5 }} />
+            <Typography fontSize={14} color="#808191">
+              {location}
+            </Typography>
+          </Stack>
+        </Stack>
 
-export default PropertyCard
+        <Box
+          px={1.5}
+          py={0.5}
+          borderRadius={1}
+          bgcolor="#dadefa"
+          height="fit-content"
+        >
+          <Typography color="#475be8" fontSize={12} fontWeight={600} >${price}</Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PropertyCard;
